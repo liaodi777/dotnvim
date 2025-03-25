b901beb2b617436c4f42ea7e94e6879d39f9046e
Liaodi777 <102370681+liaodi777@users.noreply.github.com>
2025-03-25 14:02:26 +0900

    change default colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -17,16 +17,22 @@ local plugins = {
     lazy = false,
     priority = 1000,
   },
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    lazy = false,
-    priority = 1000,
-  },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.cmd("colorscheme catppuccin")
+        end,
+    },
     {
         "cocopon/iceberg.vim",
         lazy = false,
         priority = 1000,
+        -- config = function()
+        --     vim.cmd("colorscheme iceberg")
+        -- end,
     },
     {
         "neanias/everforest-nvim",
